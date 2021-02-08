e12874d95b9eef967fe1307dfe9af995e6956520
drknsblct <drknsblct@gmail.com>
2021-02-09 01:51:28 +0200

    Added count for photos

--- gui_bot.py
+++ gui_bot.py
@@ -5,6 +5,7 @@ num = int(input("Number of open tabs: "))
 
 
 def gui_bot(num):
+    count = 0
     pyautogui.moveTo(1289, 768)  # position to move
     pyautogui.click()
     for i in range(num):
@@ -14,6 +15,8 @@ def gui_bot(num):
         pyautogui.hotkey('enter')
         pyautogui.hotkey('command', 'w')
         time.sleep(0.5)
+        count += 1
+        print(f'Image: {count}')
     print('Done')
 
 
