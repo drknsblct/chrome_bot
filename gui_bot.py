e86bfa5294414b47b375d67eea6e407dcc1a1a07
drknsblct <drknsblct@gmail.com>
2021-02-09 04:52:32 +0200

    Reformatted code blocks

--- gui_bot.py
+++ gui_bot.py
@@ -8,14 +8,15 @@ def gui_bot(num):
     count = 0
     pyautogui.moveTo(1289, 768)  # position to move
     pyautogui.click()
+
     for i in range(num):
         pyautogui.hotkey('command', 's')
         time.sleep(2.5)
-
         pyautogui.hotkey('enter')
         pyautogui.hotkey('command', 'w')
         time.sleep(0.5)
         count += 1
+
     print(f'Total files downloaded: {count}')
 
 
