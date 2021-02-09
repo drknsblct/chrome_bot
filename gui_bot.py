2d6cf33200de939ad4670c91f95cf03cc2092e1b
drknsblct <drknsblct@gmail.com>
2021-02-09 04:48:55 +0200

    Added count for photos

--- gui_bot.py
+++ gui_bot.py
@@ -3,20 +3,6 @@ import pyautogui
 
 num = int(input('Number of open tabs: '))
 
-# while True:
-
-
-# makes input lowercase and strips whitespace
-# answer = input('Are there videos in tabs? [y/n] ').lower().strip()
-# if answer[0] == 'y':
-#     sleep_timer = 2.5
-#     break
-# elif answer[0] == 'n':
-#     sleep_timer = 1.5
-#     break
-# else:
-#     continue
-
 
 def gui_bot(num):
     count = 0
@@ -24,7 +10,6 @@ def gui_bot(num):
     pyautogui.click()
     for i in range(num):
         pyautogui.hotkey('command', 's')
-        # time.sleep(sleep_timer)
         time.sleep(2.5)
 
         pyautogui.hotkey('enter')
