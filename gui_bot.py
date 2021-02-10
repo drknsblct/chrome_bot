7b7d86c935cec10bd373633088e5af18c79b44f5
drknsblct <drknsblct@gmail.com>
2021-02-10 04:47:41 +0200

    Added function for photos only

--- gui_bot.py
+++ gui_bot.py
@@ -1,24 +1,52 @@
-import time
 import pyautogui
 
-num = int(input('Number of open tabs: '))
 
-
-def gui_bot(num):
+def photos_and_videos(num):
     count = 0
     pyautogui.moveTo(1289, 768)  # position to move
     pyautogui.click()
 
     for i in range(num):
         pyautogui.hotkey('command', 's')
-        time.sleep(2.5)
+        pyautogui.sleep(2)
         pyautogui.hotkey('enter')
         pyautogui.hotkey('command', 'w')
-        time.sleep(0.5)
+        pyautogui.sleep(0.5)
+        count += 1
+
+    print(f'Total files downloaded: {count}')
+
+
+def photos(num):
+    count = 0
+    pyautogui.moveTo(60, 60)  # activates browser window
+    pyautogui.click()
+
+    for i in range(num):
+        pyautogui.moveTo(1070, 1030)
+        pyautogui.click()
+        pyautogui.sleep(1)
+        pyautogui.hotkey('command', 's')
+        pyautogui.sleep(1)
+        pyautogui.hotkey('enter')
+        pyautogui.hotkey('command', 'w')
+        pyautogui.sleep(0.1)
+        pyautogui.hotkey('command', 'w')
         count += 1
 
     print(f'Total files downloaded: {count}')
 
 
 if __name__ == '__main__':
-    gui_bot(num)
+    num = int(input('Number of open tabs: '))
+
+    while True:
+        answer = int(input('Photos[1] or Photos/Videos[2]? '))
+        if answer == 1:
+            photos(num)
+            break
+        elif answer == 2:
+            photos_and_videos(num)
+            break
+        else:
+            continue
